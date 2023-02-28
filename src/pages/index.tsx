7ba7d9d3036990dfbc82971b6288fd584fea8511
guirdy <guilherme.gl1997@gmail.com>
2023-02-28 15:38:06 -0300

    feat: stitches styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
     <>
-      <h1>Hello Next</h1>
+      <Button>
+        <span>Testedassd</span>
+        Enviar
+      </Button>
     </>
   )
 }
