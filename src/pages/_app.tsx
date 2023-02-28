ff792b4c7f3cf9f7699ad7fec7d42634b05665c8
guirdy <guilherme.gl1997@gmail.com>
2023-02-28 15:55:18 -0300

    feat: page header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app"
+import logoImg from "../assets/logo.svg"
+
+import Image from 'next/image'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} width={100} height={100} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
